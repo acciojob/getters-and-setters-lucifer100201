dc0277cd80ecd39d2244796a9d58927c832f37ca
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-04-17 22:53:20 -0600

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name,
+		this.age=age
+	}
 
-class Student extends Person {}
+	get name(){
+		return this.name;
+	}
 
-class Teacher extends Person {}
+	get age(){
+		return this.age;
+	}
+
+	set age(age){
+	   return this.age
+	}
+	
+	
+}
+
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`);
+}
 
 // Do not change the code below this line
 window.Person = Person;
